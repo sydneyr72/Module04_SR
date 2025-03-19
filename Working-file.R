6707fa9bee07b18dc9d1ba8c3fc7b7f828763408
Sydney Russo <Sydney.russo@my.jcu.edu.au>
2025-03-19 10:39:45 +1000

    first time Saving the working space

---
+++ Working-file.R
@@ -0,0 +1,7 @@
+## install if needed (do this exactly once):
+## install.packages("usethis")
+
+library(usethis)
+use_git_config(user.name = "Sydney Russo", user.email = "Sydney.russo@my.jcu.edu.au")
+
+usethis::use_github()
