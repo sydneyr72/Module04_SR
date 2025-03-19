f8c236daa90aed8218065edeec906857b2e26a87
Sydney Russo <Sydney.russo@my.jcu.edu.au>
2025-03-19 12:48:27 +1000

    saving file. I am up to 2.22.2 in the workbook

--- code/Working-file.R
+++ code/Working-file.R
@@ -45,6 +45,61 @@ ggplot(data = mpg) +
 #there are scales, shrinking (will shrink scales to fit output of statistics not raw data if true, if false it will be a range of raw data before statistical summary), switch function allows for the X and Y to switch to top, bottom, right or left. there are many more aswell.
 
 
+####Fitting Lines####
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes (x=displ, y=hwy))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy))+
+  geom_smooth(mapping = aes(x=displ, y=hwy, linetype = drv))
+
+#setting the group aesthetic
+ggplot(data = mpg)+
+  geom_smooth(mapping=aes(x=displ, y=hwy, group=drv))
+
+#changing the colour
+ggplot(data=mpg)+
+  geom_smooth(
+    mapping = aes(x = displ, y = hwy, color = drv),
+    show.legend=FALSE)
+
+#mapping multiple geoms on one plot
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy))+
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+#an easier way to do above if I wanted to change anything
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy))+
+  geom_point()+
+  geom_smooth()
+
+#adding colors
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy))+
+  geom_point(mapping = aes(color=class))+
+  geom_smooth(data = filter (mpg, class =="subcompact"), se =FALSE)
+
+####Excercise####
+#1. line: geom_line  , Boxplot:geom_boxplot  , Histogram: geom_histogram, an area chart: geom_area
+#2. I predict that the graphs will all look different becuase they are asking for the data to be laid out in different forms.
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy))+
+  geom_line()
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy))+
+  geom_boxplot()
+#3. I would assume that these two graphs are saying the same thing just in 2 different ways. and yes they are the same.
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy))+
+  geom_point()+
+  geom_smooth()
+ggplot(data = mpg)+
+  geom_point(mapping = aes(x = displ, y = hwy))+
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+####plotting statistics####
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x =cut))
+
+
+
 
 
 
