b97ffa995a7510651beee6e4327cafbf43f0eccc
Sydney Russo <Sydney.russo@my.jcu.edu.au>
2025-03-19 11:49:48 +1000

    Saving files with graphs

--- code/Working-file.R
+++ code/Working-file.R
@@ -23,5 +23,32 @@ ggplot(data = mpg) +
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, colour = displ < 5))
 
+####using Facet Wraps####
+ggplot(data=mpg) + 
+  geom_point(mapping =aes(x = displ, y = hwy)) +
+  facet_wrap (~class, nrow = 2)
+
+####using facet grids####
+#this allows the facet to split by more than one variable
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x=displ, y=hwy, colour = class))+
+  facet_grid(drv ~ cyl)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x=displ, y=hwy, colour = class))+
+  facet_grid(.~ cyl)
+
+####Exercise:####
+#what does nrow do? what does ncol do? what other options control the layout of the individual panels?
+#nrow and ncol are the number or rows and columns that are present
+#there are scales, shrinking (will shrink scales to fit output of statistics not raw data if true, if false it will be a range of raw data before statistical summary), switch function allows for the X and Y to switch to top, bottom, right or left. there are many more aswell.
+
+
+
+
+
+
+
 
              
