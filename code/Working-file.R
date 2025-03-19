f9ad01ac2a8172c38ecde63c293f0a7b65eed985
Sydney Russo <Sydney.russo@my.jcu.edu.au>
2025-03-19 11:28:03 +1000

    Saving playing around with the graphs

--- code/Working-file.R
+++ code/Working-file.R
@@ -7,10 +7,21 @@ use_git_config(user.name = "Sydney Russo", user.email = "Sydney.russo@my.jcu.edu
 usethis::use_github()
 install.packages("tidyverse")
 library(tidyverse)
+
 mpg
 
 ####Creating my First Plot####
 ggplot(data = mpg) + 
-  geom_point(mapping = aes(x = displ, y = hwy))
+  geom_point(mapping = aes(x = displ, y = hwy, colour=class))
 #this graph shows a negative relationship between engine size and fuel efficiency
 
+#changing the appearance of the graph
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
+
+#what happens if you map an aesthetic to something other than a variable name
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, colour = displ < 5))
+
+
+             
